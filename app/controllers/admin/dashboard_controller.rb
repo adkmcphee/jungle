453b7d9a75ddd588243e70fac690bc9fa8ce6676
Adam McPhee <adkmcphee@gmail.com>
2023-06-01 00:42:37 +0000

    add dashboard statistics for admin

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['Username'], password: ENV['Password']
 
   def show
+    @products_count = Product.count
+    @categories_count = Category.count
   end
-
 end
