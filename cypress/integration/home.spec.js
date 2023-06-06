647e661095342f6bab5a85a8689b68a8a7285992
Adam McPhee <adkmcphee@gmail.com>
2023-06-06 20:26:39 +0000

    add cypress tests

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,11 @@
+describe("Home Page", () => {
+  it("Visits the home page", () => {
+    cy.visit("http://localhost:3000/");
+  });
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+  it("There is 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
+});
